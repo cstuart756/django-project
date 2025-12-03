04bdeaee1de6a7cd871f322e30ed645bdde4505d
cstuart756 <stujjc@yahoo.co.uk>
2025-12-03 20:13:49 +0000

    Add Cart URLs

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:category_slug>/', views.product_list, name='product_list'),
     path('product/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
