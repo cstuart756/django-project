b39c772bcec0ae70590bb9efc5e0ee4475215240
cstuart756 <stujjc@yahoo.co.uk>
2025-12-03 18:37:41 +0000

    Add Admin Setup

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product, Order
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'available')
+    list_filter = ('available', 'created', 'updated')
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'paid', 'created')
+    list_filter = ('paid', 'created', 'updated')
+
