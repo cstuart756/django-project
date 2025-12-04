d80fb2e3e6f9796b85a316a3553664617c33623a
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 11:21:13 +0000

    Add URL for Search

--- store/urls.py
+++ store/urls.py
@@ -37,3 +37,6 @@ urlpatterns += [
     path('products/', views.product_list, name='product_list'),
     path('products/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
 ]
+urlpatterns += [
+    path('search/', views.search_products, name='search_products'),
+]
