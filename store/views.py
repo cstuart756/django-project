6052b4392091c587160d10cb6054389cdc12478e
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 10:08:11 +0000

    Create a Registration Form

--- store/views.py
+++ store/views.py
@@ -61,3 +61,16 @@ def home(request):
         'query': query
     }
     return render(request, 'store/home.html', context)
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
