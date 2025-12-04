5554df78201478c729ffd44d4bb73d2a356d49e5
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 10:09:35 +0000

    Create Registration View

--- store/views.py
+++ store/views.py
@@ -61,16 +61,18 @@ def home(request):
         'query': query
     }
     return render(request, 'store/home.html', context)
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'store/register.html', {'form': form})
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
