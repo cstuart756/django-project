89c4e08abece8fc1a306d0fd752be511dec5bc87
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 10:19:10 +0000

    Update Order Model

--- store/models.py
+++ store/models.py
@@ -35,3 +35,15 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
 
+from django.contrib.auth.models import User
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order {self.id}'
