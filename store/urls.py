e926fa6fb95514356c309b48709e29b84747b9d0
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 11:37:45 +0000

    Update URLs for Authentication

--- store/urls.py
+++ store/urls.py
@@ -43,3 +43,11 @@ urlpatterns += [
 urlpatterns += [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns += [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='store/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='product_list'), name='logout'),
+]
