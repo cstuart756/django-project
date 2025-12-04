cd3c1d7a54797de838a6ad5690e80d63f757c378
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 10:23:13 +0000

    Add Profile / Order History View

--- store/views.py
+++ store/views.py
@@ -95,3 +95,9 @@ def checkout(request):
         form = OrderCreateForm()
 
     return render(request, 'store/checkout.html', {'cart': cart, 'form': form})
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def profile(request):
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'store/profile.html', {'orders': orders})
