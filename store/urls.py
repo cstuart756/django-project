62c3d9109026582fe4931644f4e500d98e825b49
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 11:51:15 +0000

    Add URL for My Orders

--- store/urls.py
+++ store/urls.py
@@ -51,3 +51,6 @@ urlpatterns += [
     path('login/', auth_views.LoginView.as_view(template_name='store/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='product_list'), name='logout'),
 ]
+urlpatterns += [
+    path('my-orders/', views.my_orders, name='my_orders'),
+]
