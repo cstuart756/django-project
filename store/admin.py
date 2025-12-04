dbaaf168e06d827e7e3622aab42b63bc8a415579
cstuart756 <stujjc@yahoo.co.uk>
2025-12-04 11:54:48 +0000

    Register Models in Admin

--- store/admin.py
+++ store/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
-from .models import Category, Product, Order
+from .models import Category, Product, Order, OrderItem
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'available')
-    list_filter = ('available', 'created', 'updated')
-    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'category', 'price', 'stock', 'available')
+    list_filter = ('available', 'category')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'email', 'paid', 'created')
-    list_filter = ('paid', 'created', 'updated')
-
+    list_display = ('id', 'user', 'created_at', 'total_price', 'completed')
+    list_filter = ('completed', 'created_at')
+    search_fields = ('user__username', 'id')
+    inlines = [OrderItemInline]
